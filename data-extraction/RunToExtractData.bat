bea925b2941259b0c4a775cfb94cefb5debff627
Richard Williams <richard.williams2@manchester.ac.uk>
2020-03-27 15:27:03 +0000

    Latest data

--- data-extraction/RunToExtractData.bat
+++ data-extraction/RunToExtractData.bat
@@ -1,14 +1,10 @@
+@ECHO OFF
 SET SMASH.DB=PatientSafety_Records
 
-forfiles /p sql-queries /s /m covid*.sql /c "cmd /c sqlcmd -E -d %SMASH.DB% -i @path -W -s^",^" -h -1 -o data\@path.txt"
+REM for each sql file execute against db
+forfiles /p sql-queries /s /m covid*.sql /c "cmd /c sqlcmd -E -d %SMASH.DB% -i @path -W -s , -h -1 -o @fname"
 
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-tiredness.sql -W -s"," -h -1 -o data\covid.tiredness.txt
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-cough.sql -W -s"," -h -1 -o data\covid.cough.txt
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-dry-cough.sql -W -s"," -h -1 -o data\covid.dry-cough.txt
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-dry-or-chronic-cough.sql -W -s"," -h -1 -o data\covid.dry-or-chronic-cough.txt
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-high-temperature.sql -W -s"," -h -1 -o data\covid.high-temperature.txt
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-runny-nose.sql -W -s"," -h -1 -o data\covid.runny-nose.txt
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-blocked-nose.sql -W -s"," -h -1 -o data\covid.blocked-nose.txt
-REM sqlcmd -E -d %SMASH.DB% -i sql-queries\covid-symptoms-sore-throat.sql -W -s"," -h -1 -o data\covid.sore-throat.txt
+REM each output file move to data directory and add a .txt extension
+forfiles /p sql-queries /s /m covid* /c "cmd /c if not @ext==0x22sql0x22 move /y @path ../data/@fname.txt"
 
 pause
